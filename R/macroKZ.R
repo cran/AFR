ec35f46da1a8ee7ea7b42bceac7ad80cf3b8180e
Alua Makhmetova <alua.makhmetova@gmail.com>
2022-10-11 14:52:33 +0000

    version 0.2.1

--- R/macroKZ.R
+++ R/macroKZ.R
@@ -1,6 +1,6 @@
 
 #' @title macroKZ dataset
-#' @format A time series data frame of 50 quarterly observations of 49 macroeconomic and 11 financial parameters for 2010-2022 period.
+#' @format A time series data frame of 50 quarterly observations of 50 macroeconomic and 10 financial parameters for 2010-2022 period.
 #' @source Bureau of National statistics, Agency for Strategic planning and reforms of the Republic of Kazakhstan
 #' @references The Agency of the Republic of Kazakhstan for Regulation and Development of Financial Market
 "macroKZ"
